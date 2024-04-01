fcb49dfc7638b6ad42c860d1af39320cda02585b
hyen <honghyen47@gmail.com>
2024-04-01 11:38:17 +0900

    first commit

---
+++ week2/src/week2_0401/Calculator.java
@@ -0,0 +1,13 @@
+package week2_0401;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		Integer.parseInt(sc.nextLine());
+
+	}
+
+}
